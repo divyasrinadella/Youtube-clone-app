49d1c0adf6c4f921010ce8931ea499f7403b4d89
Divya Sri Nadella <137441777+divyasrinadella@users.noreply.github.com>
2024-04-02 14:26:06 -0400

    Update sonarqubeAnalysis.groovy

--- vars/sonarqubeAnalysis.groovy
+++ vars/sonarqubeAnalysis.groovy
@@ -1,6 +1,6 @@
 def call() {
   withSonarQubeEnv('sonar-scanner') {
-      sh '''$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=Netflix \
-      -Dsonar.projectkey=Netflix '''
+      sh '''$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=Yahoo \
+      -Dsonar.projectkey=Yahoo '''
   }
 }
