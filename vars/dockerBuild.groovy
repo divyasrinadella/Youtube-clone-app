1afe9d75c128dd7535a7e1251a35164e76f95e86
Divya Sri Nadella <137441777+divyasrinadella@users.noreply.github.com>
2024-04-02 18:04:28 -0400

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,6 +1,6 @@
 def call(String dockerHubUsername, String imageName) {
   sh "docker build --build-arg REACT_APP_RAPID_API_KEY=a999cf73b0msheff995319530b6dp1de10bjsn1c33561be4cf -t ${imageName} ."
-  sh "docker tag $(imageName} ${dockerHubUsername}/${imageName}:latest"
+  sh "docker tag ${imageName} ${dockerHubUsername}/${imageName}:latest"
   withDockerRegistry([url: 'https::/index.docker.io/v1/', credentialsId: 'docker']) {
      sh "docker push ${dockerHubUsername}/${imageName}:latest" 
  }
